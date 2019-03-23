d3c54f69946c0590a0e1f915a9d23c4533910778
Clouding <ghost0436@gmail.com>
2019-03-23 23:40:55 +0800

    Implement has attributes feature

---
+++ src/HasAttributes.php
@@ -0,0 +1,163 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Clouding\HasAttributes;
+
+use InvalidArgumentException;
+
+trait HasAttributes
+{
+    /**
+     * The class's attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [];
+
+    /**
+     * Create a new has attributes instance.
+     *
+     * @param array $attributes
+     */
+    public function __construct(array $attributes = [])
+    {
+        $this->setAttributes($attributes);
+    }
+
+    /**
+     * Set attribute.
+     *
+     * @param string $key
+     * @param mixed  $value
+     */
+    public function setAttribute(string $key, $value)
+    {
+        $this->checkAttribute($key, $value);
+
+        $this->attributes[$key] = $value;
+    }
+
+    /**
+     * Set attributes.
+     *
+     * @param array $attributes
+     */
+    public function setAttributes(array $attributes)
+    {
+        foreach ($attributes as $key => $value) {
+            $this->setAttribute($key, $value);
+        }
+    }
+
+    /**
+     * Check key is valid.
+     *
+     * @param string $key
+     *
+     * @param mixed  $value
+     */
+    protected function checkAttribute(string $key, $value)
+    {
+        if (!isset($this->define)) {
+            return;
+        }
+
+        if (!isset($this->define[$key])) {
+            throw new InvalidArgumentException("Key [$key] is not defined");
+        }
+
+        $define = $this->define[$key];
+
+        switch ($define) {
+            case 'string':
+                $this->checkAttributeType('string', $value);
+                return;
+            case 'int':
+            case 'integer':
+                $this->checkAttributeType('integer', $value);
+                return;
+            case 'bool':
+            case 'boolean':
+                $this->checkAttributeType('boolean', $value);
+                return;
+            case 'object':
+                $this->checkAttributeType('object', $value);
+                return;
+            case 'array':
+                $this->checkAttributeType('array', $value);
+                return;
+            case 'real':
+            case 'float':
+            case 'double':
+                $this->checkAttributeType('double', $value);
+                return;
+        }
+
+        // Check value is instance of define class or interface
+        if (class_exists($define) || interface_exists($define)) {
+            if ($value instanceof $define) {
+                return;
+            }
+
+            if (is_object($value)) {
+                $className = get_class($value);
+
+                throw new InvalidArgumentException("Object [$className] is not instance of define class [$define]");
+            }
+
+            throw new InvalidArgumentException("Value [$value] is not instance of define class [$define]");
+        }
+
+        throw new InvalidArgumentException("Define type [$define] is not supported");
+    }
+
+    /**
+     * Check attribute type is correct.
+     *
+     * @param string $type
+     * @param mixed  $value
+     */
+    protected function checkAttributeType(string $type, $value)
+    {
+        if (gettype($value) === $type) {
+            return;
+        }
+
+        throw new InvalidArgumentException("Value [$value] is not equals to define type [$type]");
+    }
+
+    /**
+     * Get attribute.
+     *
+     * @param string $key
+     * @param null   $default
+     * @return mixed|null
+     */
+    public function getAttribute(string $key, $default = null)
+    {
+        return $this->attributes[$key] ?? $default;
+    }
+
+    /**
+     * Dynamic get attribute.
+     *
+     * @param string $key
+     * @return mixed|null
+     */
+    public function __get(string $key)
+    {
+        return $this->getAttribute($key);
+    }
+
+    /**
+     * Dynamic set attribute.
+     *
+     * @param string $key
+     * @param  mixed $value
+     */
+    public function __set(string $key, $value)
+    {
+        $this->setAttribute($key, $value);
+    }
+}
---
+++ tests/HasAttributesTest.php
@@ -0,0 +1,247 @@
+<?php
+declare(strict_types=1);
+
+namespace Clouding\HasAttributes\Tests;
+
+use Clouding\HasAttributes\HasAttributes;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use ReflectionObject;
+use stdClass;
+
+class HasAttributesTest extends TestCase
+{
+    public function testSetAttribute()
+    {
+        $class = new class()
+        {
+            use HasAttributes;
+        };
+        $class->setAttribute('color', 'red');
+
+        $this->assertEquals(
+            ['color' => 'red'],
+            $this->makePublic($class, 'attributes')->getValue($class)
+        );
+    }
+
+    public function testSetAttributes()
+    {
+        $class = new class()
+        {
+            use HasAttributes;
+        };
+        $class->setAttributes([
+            'color' => 'red',
+            'number' => 10,
+            'hello' => 'world',
+        ]);
+
+        $this->assertEquals([
+            'color' => 'red',
+            'number' => 10,
+            'hello' => 'world',
+        ], $this->makePublic($class, 'attributes')->getValue($class));
+    }
+
+    public function testDynamicSetAttribute()
+    {
+        $class = new class()
+        {
+            use HasAttributes;
+        };
+        $class->color = 'red';
+
+        $this->assertEquals(
+            ['color' => 'red'],
+            $this->makePublic($class, 'attributes')->getValue($class)
+        );
+    }
+
+    public function testConstructor()
+    {
+        $class = new class([
+            'color' => 'red',
+            'number' => 10,
+            'hello' => 'world',
+        ])
+        {
+            use HasAttributes;
+        };
+
+        $this->assertEquals([
+            'color' => 'red',
+            'number' => 10,
+            'hello' => 'world',
+        ], $this->makePublic($class, 'attributes')->getValue($class));
+    }
+
+    public function testGetAttribute()
+    {
+        $class = new class()
+        {
+            use HasAttributes;
+        };
+
+        $this->assertNull($class->getAttribute('color'));
+        $this->assertSame('red', $class->getAttribute('color', 'red'));
+    }
+
+    public function testDynamicGetAttribute()
+    {
+        $class = new class([
+            'color' => 'red',
+        ]) {
+            use HasAttributes;
+        };
+
+        $this->assertNull($class->fruit);
+        $this->assertSame('red', $class->color);
+    }
+
+    public function testDefineAndSetNotExistsKeyException()
+    {
+        $class = new class()
+        {
+            use HasAttributes;
+
+            protected $define = [
+                'color' => 'string',
+            ];
+        };
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Key [fruit] is not defined');
+
+        $class->setAttribute('fruit', 'banana');
+    }
+
+    public function testDefineAndSetAttributeNoException()
+    {
+        $class = new class()
+        {
+            use HasAttributes;
+
+            protected $define = [
+                'color' => 'string',
+                'number' => 'int',
+                'number2' => 'integer',
+                'isAdmin' => 'bool',
+                'isAdmin2' => 'boolean',
+                'posts' => 'object',
+                'article' => 'array',
+                'score' => 'real',
+                'score2' => 'float',
+                'score3' => 'double',
+                'pig' => Eatable::class,
+            ];
+        };
+
+        $class->setAttributes([
+            'color' => 'red',
+            'number' => 123,
+            'number2' => 123,
+            'isAdmin' => true,
+            'isAdmin2' => true,
+            'posts' => new stdClass(),
+            'article' => ['a', 'b'],
+            'score' => 1.0,
+            'score2' => 1.0,
+            'score3' => 1.0,
+            'pig' => new Pig(),
+        ]);
+
+        $this->assertTrue(true);
+    }
+
+    public function testDefineAndSetCommonTypeException()
+    {
+        $class = new class()
+        {
+            use HasAttributes;
+
+            protected $define = [
+                'color' => 'string',
+            ];
+        };
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Value [123] is not equals to define type [string]');
+
+        $class->setAttribute('color', 123);
+    }
+
+    public function testDefineAndSetObjectNotInstanceOfTypeException()
+    {
+        $class = new class()
+        {
+            use HasAttributes;
+
+            protected $define = [
+                'pig' => Eatable::class,
+            ];
+        };
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage(
+            'Object [stdClass] is not instance of define class [Clouding\HasAttributes\Tests\Eatable]'
+        );
+
+        $class->setAttribute('pig', new stdClass());
+    }
+
+    public function testDefineAndSetValueNotInstanceOfTypeException()
+    {
+        $class = new class()
+        {
+            use HasAttributes;
+
+            protected $define = [
+                'pig' => Eatable::class,
+            ];
+        };
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage(
+            'Value [eat] is not instance of define class [Clouding\HasAttributes\Tests\Eatable]'
+        );
+
+        $class->setAttribute('pig', 'eat');
+    }
+
+    public function testDefineNotSupportedTypeException()
+    {
+        $class = new class()
+        {
+            use HasAttributes;
+
+            protected $define = [
+                'pig' => 'foo',
+            ];
+        };
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Define type [foo] is not supported');
+
+        $class->setAttribute('pig', 'eat');
+    }
+
+    protected function makePublic($obj, $property)
+    {
+        $reflect = new ReflectionObject($obj);
+        $property = $reflect->getProperty($property);
+        $property->setAccessible(true);
+
+        return $property;
+    }
+}
+
+interface Eatable
+{
+
+}
+
+class Pig implements Eatable
+{
+
+}
